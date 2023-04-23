d1b728c8f32ae11d941400edab60fb8bb9dbab14
nbigger <131599905+nbigger@users.noreply.github.com>
2023-04-23 22:09:25 +0800

    Create mydefine.h
    add header new first

---
+++ mydefine.h
@@ -0,0 +1,12 @@
+#ifndef mydefine_h
+#define mydefine_h
+
+//add by nbigger 2023.04.23
+
+#define projectname  "helloai"
+#define projectdata  "2023.04.23"
+#define projectversion "V1.0.0"
+
+#define USE_LINUX_MACRO
+
+#endif
